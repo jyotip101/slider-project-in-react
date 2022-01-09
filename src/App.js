d8d4579bfb5aee3016385a412ac45690a29810aa
jyoti patel <jyotipatelfyit77100@gmail.com>
2022-01-09 14:26:44 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import './App.css'
-import Tabs from './componantes/Tabs'
 
 function App() {
   return (
     <div className='App'>
-      <Tabs />
       <br />
       <br />
       <br />
       <br />
       <footer>
         created by -
-        <a href='https://github.com/jyotip101/menu-page-in-react'>Jyoti-p</a>
+        <a href='https://github.com/jyotip101/slider-project-in-react'>
+          Jyoti-p
+        </a>
       </footer>
     </div>
   )
